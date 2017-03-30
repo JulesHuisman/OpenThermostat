c083fb513ddaa961f8ec5fdb83b163c3b77921b6
JulesHuisman <huisman.jules@gmail.com>
2017-03-30 12:43:52 +0200

    Added thermostat screen object

---
+++ OpenThermostatScreen.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+#include "OpenThermostatScreen.h"
+
+OpenThermostatScreen::OpenThermostatScreen()
+{
+
+}
+
+void OpenThermostatScreen::begin()
+{
+  DEBUG_PRINTLN("Screen Begin");
+}
